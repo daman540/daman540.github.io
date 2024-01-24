7b47bc43bf0fb55ff251155778e5b7e4cf13021c
Dhruve Mistry <d_m704@txstate.edu>
2024-01-24 12:38:37 -0600

    Changes to darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -1,4 +1,11 @@
+let isDark = false;
 function darkMode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
+    var element = document.getElementById("darkmode");
+    
+    if(!isDark){
+        element.classList.toggle("dark-mode");
+
+        element.classList.toggle("dark-mode-text")
+    }
+    
 }

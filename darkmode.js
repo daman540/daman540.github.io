46cbf925b3fadd6a4cfac3d38192e88079f55bdd
Dhruve Mistry <96855692+daman540@users.noreply.github.com>
2024-01-24 15:01:06 -0600

    Need to do

--- darkmode.js
+++ darkmode.js
@@ -1,11 +1,7 @@
-let isDark = false;
 function darkMode() {
     var element = document.getElementById("darkmode");
     
-    if(!isDark){
-        element.classList.toggle("dark-mode");
-
-        element.classList.toggle("dark-mode-text")
-    }
+    element.classList.toggle("dark-mode");
+    element.classList.toggle("dark-mode-text")
     
 }

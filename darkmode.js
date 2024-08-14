44d5744816c06ced0f4c5a87926bbeea1beda8dd
Dhruve Mistry <d_m704@txstate.edu>
2024-08-14 13:29:11 -0500

    Reverting

---
+++ darkmode.js
@@ -0,0 +1,7 @@
+function darkMode() {
+    var element = document.getElementById("darkmode");
+    
+    element.classList.toggle("dark-mode");
+    element.classList.toggle("dark-mode-text")
+    
+}

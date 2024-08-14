b358f2e30968bd0891aa8d33e8b5cde40e8df920
Dhruve Mistry <d_m704@txstate.edu>
2024-08-14 13:23:07 -0500

    Complete Redesign

--- darkmode.js
+++
@@ -1,7 +0,0 @@
-function darkMode() {
-    var element = document.getElementById("darkmode");
-    
-    element.classList.toggle("dark-mode");
-    element.classList.toggle("dark-mode-text")
-    
-}
